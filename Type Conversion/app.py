863ec487910997fa06997aafbd3868c4763f08ba
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 06:56:50 +0000

    Type Conversion

---
+++ Type Conversion/app.py
@@ -0,0 +1,7 @@
+birth_year = input('Birth year: ')
+print(type(birth_year))
+
+age = 2025 - int(birth_year)
+print(age)
+print(type(age))
+
---
+++ Type Conversion/ex.py
@@ -0,0 +1,6 @@
+'''Ask a user their weight(in pounds),convert it to
+Kilograms and print on the terminal '''
+
+weight_lbs = input('Weeight (lbs): ')
+weight_kg = float(weight_lbs) * 0.45
+print(weight_kg)
