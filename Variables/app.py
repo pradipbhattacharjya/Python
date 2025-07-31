24f955a387998a50441397762fad1195e86507f9
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 06:58:13 +0000

    Variables

---
+++ Variables/app.py
@@ -0,0 +1,6 @@
+price = 10
+price = 20
+rating = 4.9
+name = 'Pradip'
+is_published = False
+print(price)
---
+++ Variables/ex.py
@@ -0,0 +1,8 @@
+'''We check in a patient named john Smith.
+He's 20 years old and is a new patient.'''
+
+full_name = 'John Smith'
+age = 20
+is_new = True
+
+print(age)
