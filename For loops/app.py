d48c875e722d5b55a8e61a6ffa807d3634eef7b4
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 13:56:51 +0000

    For loop

---
+++ For loops/app.py
@@ -0,0 +1,13 @@
+for item in ['Mosh', 'John','Sarah']:
+    print(item)
+
+for item in [1, 2, 3, 4]:
+    print(item)
+
+
+for item in range(5, 10):
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
+
