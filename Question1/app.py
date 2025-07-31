cef3783a90b030b742ebf21565d94b46d6a8eeb7
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 12:34:33 +0000

    Question one

---
+++ Question1/app.py
@@ -0,0 +1,11 @@
+'''Weight Converter '''
+
+Weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+
+if unit.upper() == "L":
+    converted = Weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = Weight / 0.45
+    print(f"You are {converted} pounds")
