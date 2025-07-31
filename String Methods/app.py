65a3238ffbb93f6822e54e8de57a3ecc5b2f04c9
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 06:54:11 +0000

    String Methods

---
+++ String Methods/app.py
@@ -0,0 +1,10 @@
+course = 'Python for Beginners'
+print(len(course))
+
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.find('Beginners'))
+print(course.replace('Beginners','Absolute Beginners'))
+print('python' in course)
+print(len(course))
