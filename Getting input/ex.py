48dc2cc5df5b87a25ce870ec1703119dcbd1cf40
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-07-31 07:03:27 +0000

    Getting input

---
+++ Getting input/ex.py
@@ -0,0 +1,7 @@
+'''Ask tow questions: person's name and favourite color.
+Then,print a messsage like ''Mosh likes blue'''
+
+name = input('What is your name? ')
+color =input('What is your favourite color')
+
+print(name + ' like ' +  color)
