ce1e719e6c7380f0b363101335b297dad4aaa444
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-01 03:39:01 +0000

    Lists

---
+++ Lists/app.py
@@ -0,0 +1,4 @@
+names = ['Johan', 'Bob','Mosh', 'Sarah', 'Mary']
+print(names[0])
+print(names[2:4])
+print(names[2:])
---
+++ Lists/app2.py
@@ -0,0 +1,9 @@
+#Write a program to find the largest number in a list.
+
+numbers = [3, 6, 2, 8, 4, 10]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
+
