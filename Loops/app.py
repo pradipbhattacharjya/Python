257e94ef0b91e8da1af6008b0b46ce29778b79d1
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-02 07:29:23 +0000

    While loop

---
+++ Loops/app.py
@@ -0,0 +1,14 @@
+# Need for loops
+# While loop
+# For Loop
+
+# While loop example -> program to print the table
+number = int(input('Enter the number'))
+
+i = 1
+while i <11:
+    print(number,'*',i,'=',number * i)
+    i += 1
+# Program -> Sum of all digits of a given number
+# Program -> keep accepting numbers from users till he/she enters a 0 and then find the avg.
+
---
+++ Loops/app2.py
@@ -0,0 +1,10 @@
+#While loop with else
+
+x = 1 
+
+while x < 7:
+    print(x)
+    x +=1
+
+else:
+    print('limit crossed')
