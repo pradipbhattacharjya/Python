3ed0afbdc606e15fed547e04859dc442e55973f8
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-02 04:43:33 +0000

    Modules

---
+++ Modules/app.py
@@ -0,0 +1,20 @@
+#math
+import math
+
+print(math.factorial(5))
+print(math.floor(6.8))
+print(math.sqrt(196))
+
+#Keyword
+import keyword
+print(keyword.kwlist)
+
+#rendom
+import random
+print(random.randint(1,100))
+
+#detetime
+import datetime
+print(datetime.datetime.now())
+
+
