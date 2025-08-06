60b26eb36f95737f929d806e00bdee5307221e0b
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-06 16:03:40 +0000

    Set Functions

---
+++ Sets/app7.py
@@ -0,0 +1,8 @@
+# Set Functions
+# len/sum/min/max/sorted
+s = {3,1,4,5,2,7}
+print(len(s))
+print(sum(s))
+print(min(s))
+print(max(s))
+print(sorted(s,reverse=True))
---
+++ Sets/app9.py
@@ -0,0 +1,9 @@
+# intersection/intersection_update
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+
+s1.intersection(s2)
+
+s1.intersection_update(s2)
+print(s1)
+print(s2)
