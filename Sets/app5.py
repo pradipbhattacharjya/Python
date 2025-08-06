26737cbf086742c1e4fba46292c25d8251cf845e
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-06 15:42:43 +0000

    Deleting Items

---
+++ Sets/app5.py
@@ -0,0 +1,17 @@
+# Deleting Items
+# del
+s = {1,2,3,4,5}
+print(s)
+del s[0]
+print(s)
+# discard
+s.discard(50)
+print(s)
+# remove
+s.remove(50)
+print(s)
+# pop
+s.pop()
+# clear
+s.clear()
+print(s)
