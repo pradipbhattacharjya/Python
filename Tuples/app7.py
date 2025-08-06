125aec665364a8b9d72d67962b459686e031ac49
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-06 15:00:53 +0000

    Tuple Functions

---
+++ Tuples/app7.py
@@ -0,0 +1,16 @@
+# Tuple Functions
+# len/sum/min/max/sorted
+t = (1,2,3,4)
+print(len(t))
+print(sum(t))
+print(min(t))
+print(max(t))
+print(sorted(t,reverse=True))
+
+#count
+t2 = (1,2,3,4)
+print(t2.count(1))
+
+#index
+print(t.index(1))
+
