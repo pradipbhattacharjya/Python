b4a15ad315bc8f0e96b4cf01dbe4fba5c99ab04f
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-06 15:43:58 +0000

    Set Operation

---
+++ Sets/app6.py
@@ -0,0 +1,17 @@
+# Set Operation
+s1 = {1,2,3,4,5}
+s2 = {4,5,6,7,8}
+s1 | s2
+# Union(|)
+# Intersection(&)
+s1 & s2
+# Difference(-)
+s1 - s2
+s2 - s1
+# Symmetric Difference(^)
+s1 ^ s2
+# Membership Test
+1 not in s1
+# Iteration
+for i in s1:
+  print(i)
