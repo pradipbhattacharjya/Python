f6c049f926f05ccdcf873eca7a37a1827aebd6bd
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-07 03:29:36 +0000

    Dictionary Functions

---
+++ Dictionary/app8.py
@@ -0,0 +1,7 @@
+# Dictionary Functions
+d = {'name':'nitish','gender':'male','age':33}
+# len/sorted
+print(len(d))
+print(d)
+print(sorted(d,reverse=True))
+print(max(d))
---
+++ Dictionary/app9.py
@@ -0,0 +1,14 @@
+# items/keys/values
+d = {'name':'nitish','gender':'male','age':33}
+print(d)
+
+print(d.items())
+print(d.keys())
+print(d.values())
+
+#Update
+d1 = {1:2,3:4,4:5}
+d2 = {4:7,6:8}
+
+d1.update(d2)
+print(d1)
