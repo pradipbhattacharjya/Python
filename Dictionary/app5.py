227b3e0b22035e19f50dcc627f8b6a300bdb1f57
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-07 03:12:49 +0000

    # Editing key-value pair

---
+++ Dictionary/app5.py
@@ -0,0 +1,15 @@
+# Editing key-value pair
+
+s = {
+    'name':'nitish',
+     'college':'bit',
+     'sem':4,
+     'subjects':{
+         'dsa':50,
+         'maths':67,
+         'english':34
+     }
+}
+
+s['subjects']['dsa'] = 80
+print(s)
