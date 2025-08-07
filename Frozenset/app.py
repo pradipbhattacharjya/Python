ee7ffc248b6cde2c18b4b18c6d062f11824ed6b9
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-07 02:32:13 +0000

    frozenset

---
+++ Frozenset/app.py
@@ -0,0 +1,7 @@
+#create frozenset
+fs = frozenset([1,2,3])
+print(fs)
+
+fs1 = frozenset([1,2,3])
+fs2 = frozenset([3,4,5])
+print(fs1 | fs2)
---
+++ Frozenset/app2.py
@@ -0,0 +1,3 @@
+#2D frozenset
+fs = frozenset([1,2,frozenset([3,4])])
+print(fs)
