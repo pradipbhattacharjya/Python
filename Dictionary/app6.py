b02f6ae62a1665892ef3d0e0e3041926d1f56cc9
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-07 03:17:26 +0000

    Dictionary Operatirs

---
+++ Dictionary/app6.py
@@ -0,0 +1,17 @@
+#Dictionary Operatirs
+# Membership
+
+s = {
+    'name':'nitish',
+     'college':'bit',
+     'sem':4,
+     'subjects':{
+         'dsa':50,
+         'maths':67,
+         'english':34
+     }
+}
+
+print(s)
+
+print('name' is  s)
---
+++ Dictionary/app7.py
@@ -0,0 +1,4 @@
+d = {'name':'nitish','gender':'male','age':33}
+
+for i in d:
+  print(i,d[i])
