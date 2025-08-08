218218135311969bbd537f3f5a728e484c3e7f45
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-08 16:22:11 +0000

    Independent if-else

---
+++ if_else/app6.py
@@ -0,0 +1,14 @@
+#Independent if-else
+
+score = 50
+Submitted_project = False
+
+if score >= 90:
+    print("High Score")
+else:
+    print("Low Score")
+
+if Submitted_project:
+    print("Project is submitted")
+else:
+    print("Project is not submitted")
