21daeb1c85f6347d45a45cc04e518135021b5169
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-08 16:15:59 +0000

    Logical Operators

---
+++ if_else/app5.py
@@ -0,0 +1,15 @@
+score = 50
+Submitted_project = False
+if score >= 90 and Submitted_project:
+        print("A+")
+elif score >=90:
+     print("A")
+elif score >=80:
+    print("B")
+elif score >=70:
+    print("C")
+elif score >=60 or Submitted_project:
+
+    print("D")
+else:
+    print("F")
