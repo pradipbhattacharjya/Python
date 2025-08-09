479e67a742297f0433c0c9c91202d8505778aaa2
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-09 10:46:03 +0000

    sequences you can loop

---
+++ For loops/app4.py
@@ -0,0 +1,19 @@
+#sequences you can loop
+items = [1,2,3,4,5]
+for item in items:
+    print(f"Round: {item}")
+
+
+items = "Python"
+for item in items:
+    print(f"Round: {item}")
+
+
+for item in range(100):
+    print(f"Round: {item}")
+
+for item in range(1,6):
+    print(f"Round: {item}")
+
+for item in range(1,10,2):
+    print(f"Round: {item}")
