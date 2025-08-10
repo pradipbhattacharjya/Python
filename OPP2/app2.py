990c232470fc5484201f5bf581bf2e76fec9d446
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-10 11:04:33 +0000

    Private(like) attributes & methods

---
+++ OPP2/app2.py
@@ -0,0 +1,13 @@
+#Private(like) attributes & methods
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.__acc_pass = acc_pass 
+
+    def reset_pass(self):
+        print(self.__acc_pass)
+
+acc1 = Account("12345","ABCDE")
+
+print(acc1.acc_no)
+print(acc1.reset_pass)
