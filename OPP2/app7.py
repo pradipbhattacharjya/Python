cbd0982fd184777860835e7cd782c69e1bbd30be
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-10 11:41:49 +0000

    Super method

---
+++ OPP2/app7.py
@@ -0,0 +1,24 @@
+#Super method 
+
+class Car:
+    def __init__(self,type):
+        self.type =type
+
+    @staticmethod
+    def start():
+        print("Car started...")
+
+    @staticmethod
+    def stop():
+        print("car stiopped. ")
+
+class ToyotaCar(Car):
+    def __init__(self, name,type):
+        self.name = name
+        super().__init__(type)
+        self.name = name
+        super().start()
+
+
+car1 = ToyotaCar("prius","electric")
+print(car1.type)
