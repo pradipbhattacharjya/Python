58de66eac1e0834040fb646bd0ca4d9f5418b697
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-12 05:09:43 +0000

    list of objects

---
+++ OPP2/app21.py
@@ -0,0 +1,16 @@
+#Collection of objects
+# list of objects
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+p1 = Person('nitish','male')
+p2 = Person('ankit','male')
+p3 = Person('ankita','female')
+
+L = [p1,p2,p3]
+
+for i in L:
+  print(i.name,i.gender)
