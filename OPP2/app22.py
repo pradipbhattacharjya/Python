b638e40057987040023abef586fbacad274be213
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-12 05:10:57 +0000

    dict of objects

---
+++ OPP2/app22.py
@@ -0,0 +1,14 @@
+# dict of objects
+# list of objects
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+p1 = Person('nitish','male')
+p2 = Person('ankit','male')
+p3 = Person('ankita','female')
+
+d = {'p1':p1,'p2':p2,'p3':p3}
+
