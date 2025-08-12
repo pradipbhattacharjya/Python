3b0fe997d49b6cad3bfb6608a5e2fe3a20871b18
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-12 04:36:18 +0000

    Object ki mutability

---
+++ OPP2/app19.py
@@ -0,0 +1,17 @@
+#Object ki mutability
+
+class Person:
+
+  def __init__(self,name,gender):
+    self.name = name
+    self.gender = gender
+
+# outside the class -> function
+def greet(person):
+  person.name = 'ankit'
+  return person
+
+p = Person('nitish','male')
+print(id(p))
+p1 = greet(p)
+print(id(p1))
