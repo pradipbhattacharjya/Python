01d331d4824c0d5ba6cff5bb7fdfe596f17778aa
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-13 10:30:20 +0000

    Aggregation

---
+++ opp3/app.py
@@ -0,0 +1,38 @@
+#Aggregation
+
+class Customer:
+
+  def __init__(self,name,gender,address):
+    self.name = name
+    self.gender = gender
+    self.address = address
+
+  def print_address(self):
+    print(self.address._Address__city,self.address.pin,self.address.state)
+
+  def edit_profile(self,new_name,new_city,new_pin,new_state):
+    self.name = new_name
+    self.address.edit_address(new_city,new_pin,new_state)
+
+class Address:
+
+  def __init__(self,city,pin,state):
+      self.__city = city
+      self.pin = pin
+      self.state = state
+
+  def get_city(self):
+    return self.__city
+
+  def edit_address(self,new_city,new_pin,new_state):
+    self.__city = new_city
+    self.pin = new_pin
+    self.state = new_state
+
+add1 = Address('gurgaon',122011,'haryana')
+cust = Customer('nitish','male',add1)
+
+cust.print_address()
+
+cust.edit_profile('ankit','mumbai',111111,'maharastra')
+cust.print_address()
