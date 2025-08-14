a8466330ebb188b49bd235ebd10f24194e7404eb
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-14 05:09:01 +0000

    can super access parent ka data? using super outside the class

---
+++ opp3/app11.py
@@ -0,0 +1,21 @@
+# can super access parent ka data?
+# using super outside the class
+class Phone:
+    def __init__(self, price, brand, camera):
+        print ("Inside phone constructor")
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print ("Buying a phone")
+
+class SmartPhone(Phone):
+    def buy(self):
+        print ("Buying a smartphone")
+        # syntax to call parent ka buy method
+        print(super().brand)
+
+s=SmartPhone(20000, "Apple", 13)
+
+s.buy()
