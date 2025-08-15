dea90b90d2c72792e4fa4e6f2ed7c006c68842fc
Pradip Bhattacharjya <pradipbhattacharjya1997@gmail.com>
2025-08-15 12:23:58 +0000

    Splitting

--- Numpy Fundamentals/app9.py
+++ Numpy Fundamentals/app9.py
@@ -4,4 +4,6 @@ import numpy as np
 a4 = np.arange(12).reshape(3,4)
 a5 = np.arange(12,24).reshape(3,4)
 a5
-print(np.hsplit(a4,5))
+print(np.hsplit(a4,2))
+# vertical splitting
+print(np.vsplit(a5,3))
