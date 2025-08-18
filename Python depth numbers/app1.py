150cf6a8ef5c65afa791a3e18f39acab575c2594
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-18 18:00:38 +0530

    Numbers in depth in python

---
+++ Python depth numbers/app1.py
@@ -0,0 +1,10 @@
+import random
+print(random.random())
+print(random.randint(1, 10))
+# print(random.choice)
+
+l1 =['lemon','masala','ginger','mint']
+print(random.choice(l1))
+
+print(random.shuffle(l1))
+print(l1)
