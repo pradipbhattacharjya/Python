ea59209a82b1c71c2a491fe77459bc3b6dc5c263
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-18 17:57:58 +0530

    Numbers in depth in python

---
+++ Python depth numbers/app.py
@@ -0,0 +1,61 @@
+x = 2
+y = 3
+z = 4
+
+print(x + y)
+
+print(40 + 2.23)
+# print('hitesh' + 3)
+
+print(int(2.23))
+print(float(40))
+print('chai' + 'code')
+
+
+print(x, y, z)
+
+result = 1/3.0
+print(result)
+
+repr('chai')
+print(repr)
+
+str('chai')
+print(str)
+
+p =(1 < 2)
+print(p)
+
+s =(4.0 != 5.0)
+print(s)
+
+print(x, y, z)
+
+print(x < y < z)
+print(x < y and y < z)
+
+print(1 == 2 < 3)
+print(1 == 2 and 2 < 3)
+
+import math
+print(math.floor(3.5))
+
+print(math.trunc(2.8))
+print(2 + 1j)
+print(2 + 1j * 3)
+
+# octal letaral
+print(0o20)
+
+print(0xFF)
+print(0b1000)
+print(oct(64))
+print(hex(64))
+print(bin(64))
+print(int(64))
+
+x = 1
+x << 2
+
+
+
