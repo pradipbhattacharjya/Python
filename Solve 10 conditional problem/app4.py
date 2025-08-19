0c8c1e8f2274485b5d92d2cadc56e5d0bfc8181a
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-19 11:06:43 +0530

    Fruit Ripeness Checker

---
+++ Solve 10 conditional problem/app4.py
@@ -0,0 +1,13 @@
+#4> Fruit Ripeness Checker
+#Determine if a fruit is ripe, overripe bassed on its color.(e.g..Banana:Green - Unripe, Yellow - Rip,Brown-Overripe)
+
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
