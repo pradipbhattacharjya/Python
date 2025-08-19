c1c0e6d6f3c8dc587cc6b3b513053e24f83c8c21
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-19 16:04:33 +0530

    Reverse a string using a loop

---
+++ Solve 10 loops problem/app4.py
@@ -0,0 +1,11 @@
+#4>Reverse a S tring
+#Reverse a string using a loop.
+
+input_str = "Python"
+reversed_str = ""
+
+for char in input_str:
+    reversed_str = char + reversed_str 
+
+
+print(reversed_str)
