32eb68b37b1d9086ca0fd2a9b71fdfdea4043994
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-19 20:52:25 +0530

    Function with Multiple Parameters

---
+++ Solve 10 problems on functions/app1.py
@@ -0,0 +1,6 @@
+#2>Function with Multiple Parameters
+#Create a function that taskes two numbers as parameters and returns their sum.
+def add(numOne,numTwo):
+    return numOne + numTwo
+
+print(add(2,3))
