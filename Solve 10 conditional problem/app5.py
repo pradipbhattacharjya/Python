22773d39bb67afd1ad3c4b448d509414e29e6b07
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-19 11:22:05 +0530

    eather Activity Suggestion

---
+++ Solve 10 conditional problem/app5.py
@@ -0,0 +1,11 @@
+#5> Weather Activity Suggestion
+#Suggest an activity based on the weather(e.g..Sunny - Go for walk,RAINY-Read a book,Snowy-Build snowan)
+weather = "Sunny"
+if weather == "Sunny":
+    activity  = "Go for a walk"
+elif weather == "Rainy":
+    activity = "Read a book"
+elif weather == "Snowy":
+    activity = "Build a snowman" 
+
+print(activity)
