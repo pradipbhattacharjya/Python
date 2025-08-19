4a24049292db649515d9c4d0ab71b92a2b175e09
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-19 08:46:39 +0530

    Movie Ticket Pricing

---
+++ Solve 10 conditional problem/app1.py
@@ -0,0 +1,12 @@
+#2> Movie Ticket Pricing
+#MOvie tickets are priced on age $12 for adults(18 and over),$8 for children. Everyone gets a $2 discount on Wednesday
+
+age = 22
+day = "Wednesday"
+
+price = 12  if age >= 18 else 8
+
+if day == "Wednesday":
+    price = price - 2
+
+print("Ticket price for you is $",price)
