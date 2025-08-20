54703a345ce6ce12f781318a6f54438ade1c4994
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-20 18:03:19 +0530

    Cache Return Values

---
+++ Decorators in python/app3.py
@@ -0,0 +1,23 @@
+#3>Cache Return Values
+# Implement a decorator that caches the return values of a function, so that when it's called with the
+#ame arguments, the cached value is returned instead of re-executing the function.
+import time
+
+def cache(func):
+    cache_value = {}
+    print(cache_value)
+    def wrapper(*args):
+        if args in cache_value:
+            return cache_value[args]
+        result = func(*args)
+        cache_value[args] = result
+        return result
+    return wrapper
+
+@cache
+def long_runing_function(a, b):
+    time.sleep(4)
+    return a + b
+print(long_runing_function(2,3))
+print(long_runing_function(2,3))
+print(long_runing_function(4,3))
