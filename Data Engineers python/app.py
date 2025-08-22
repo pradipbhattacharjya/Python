eb77b61b74415afa5ad677cf98e07b91bbb9d773
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-22 09:10:21 +0530

    nested if else

---
+++ Data Engineers python/app.py
@@ -0,0 +1,12 @@
+x = 210
+
+
+if (x == 10):
+    print("x is 10")
+elif(x > 100):
+    if((x>100) & (x<200)):
+        print("Between 100 & 200")
+    else:
+        print("Greater then 200")
+else:
+    print("X is not 10")
