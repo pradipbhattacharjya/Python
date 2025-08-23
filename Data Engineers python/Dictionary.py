d30322812572c8fa03a96d6cf083f85008471a8a
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-23 08:21:22 +0530

    Dictionary

---
+++ Data Engineers python/Dictionary.py
@@ -0,0 +1,10 @@
+my_dictionary = {"x":1,"y":2,"z":3,"demo":{"a":1,"b":2}}
+
+my_dictionary["x"] = 10
+# my_dictionary.pop("z")
+
+print(my_dictionary)
+print(my_dictionary.keys())
+print(my_dictionary.values())
+print(my_dictionary.items())
+print(my_dictionary['demo']['b'])
