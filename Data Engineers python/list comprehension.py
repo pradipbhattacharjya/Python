31c8c9cf074cc3693f14e1debb3db8c576b66adb
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-23 08:05:36 +0530

    list comprehensin

---
+++ Data Engineers python/list comprehension.py
@@ -0,0 +1,12 @@
+my_list = [1,2,3,4,5,6]
+
+new_list = [i*i for i in my_list if (i % 2) == 0  if (i!=6)]
+
+print(new_list)
+
+
+
+# for i in my_list:
+#     new_list.append(i*i)
+
+# print(new_list)
