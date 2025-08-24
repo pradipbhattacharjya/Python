1c2da1dcc15301936b14bb695c825eaa21255601
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-24 17:09:03 +0530

    classmethod

---
+++ Data Engineers python/Class Methods.py
@@ -0,0 +1,34 @@
+class employee():
+    company_name = "xyz"
+
+    def __init__(self,emp_name,emp_dept):
+        self.emp_name = emp_name
+        self.emp_dept = emp_dept
+        
+    #Alternative to Class Method
+    def changes(self,new_company):
+        employee.company_name = new_company
+
+    @classmethod
+    def changesInClass(self,new_company):
+        self.company_name = new_company
+
+    def info(self):
+        print(f"Employee {self.emp_name} works for {self.emp_dept} in {self.company_name}")
+
+    #static Method
+    @staticmethod
+    def addition(x,y):
+        print(x+y)
+
+
+emp1 = employee("subha","IT")
+emp2 =employee("Pradip","HR")
+
+emp1.changesInClass("NewNewCompany")
+
+print(emp1.company_name)
+# emp1.addition(1,2)
+# emp1.changes('New company')
+
+# emp2.info()
