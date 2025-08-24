02949157b9862d1129f0e9eaf2c82ec2cc8b808a
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-24 16:50:26 +0530

    oop

---
+++ Data Engineers python/Oop.py
@@ -0,0 +1,21 @@
+class employee():
+    company_name = "xyz"
+
+    def __init__(self,emp_name,emp_dept):
+        self.emp_name = emp_name
+        self.emp_dept = emp_dept
+    
+    def changes(self,new_company):
+        employee.company_name = new_company
+
+    def info(self):
+        print(f"Employee {self.emp_name} works for {self.emp_dept} in {self.company_name}")
+
+
+emp1 = employee("subha","IT")
+emp2 =employee("Pradip","HR")
+
+emp1.changes('New company')
+
+emp2.info()
+
