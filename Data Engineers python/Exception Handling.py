10e26c027cafdd9c4ce1766535a4a89d84334df8
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-24 08:47:52 +0530

    Excetion handling

---
+++ Data Engineers python/Exception Handling.py
@@ -0,0 +1,14 @@
+#try except 
+x = "10"
+
+try:
+    if (x>10):
+        print("Greater than 10")
+
+    else:
+        print("Bla-Bla")
+
+except:
+    print("Hey, you have an error")
+
+print("Hello world")
