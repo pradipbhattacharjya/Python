e6051cd9b4f4a5ff2fd51b1d67e65aa16341e3af
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-28 17:31:54 +0530

    Comparison Operatoes

---
+++ mos/Comparison Operators.py
@@ -0,0 +1,15 @@
+#if temperature is greater than 30
+#     it's a hot day
+# othereise if it's less than 10
+#     it's a cold day
+# otherwise
+#     it's neither hot nor cold
+
+temperature = 35
+
+if temperature >= 30:
+    print("it's a hot day")
+elif temperature <= 30:
+    print("it's a cold day")
+else:
+    print("it's neither hot nor cold")
