113ab0506b8ec2a5b6e74a52d252cb7d5e4ad8d8
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-28 16:23:18 +0530

    string Methods

---
+++ mos/string Methods.py
@@ -0,0 +1,7 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('P'))
+print(course.replace('Beginners','Absolute Beginners'))
+print('python' in course)
