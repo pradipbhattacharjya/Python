d0968fe44974aa4570538a069c4998ee16406c92
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-28 16:00:18 +0530

    string

---
+++ mos/String.py
@@ -0,0 +1,21 @@
+course = "Python's Course for Beginners"
+print(course)
+
+sp = '''
+hi jhon,
+
+Here is our first email to you.
+Thank you
+'''
+
+print(sp)
+
+course = "Python for Beginners"
+print(course[0])
+print(course[0:3])
+print(course[1:])
+print(course[:5])
+print(course[:])
+
+another = course[:]
+print(another)
