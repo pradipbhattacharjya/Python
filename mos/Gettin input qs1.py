b56a241ccdb08cd966a7c7d314d449a8f5178f44
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-28 15:33:31 +0530

    Gettin input qs

---
+++ mos/Gettin input qs1.py
@@ -0,0 +1,7 @@
+#Ask two questions:person's name and favourite color.
+# Then,print a message like "Mosh likes blue"
+
+name = input("What is your name? ")
+color = input("your favourit color.")
+
+print(name +" likes " + color)
