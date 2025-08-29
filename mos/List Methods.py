3d20785292f11f0b1b4d66e937184bc503a3922c
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-29 17:06:19 +0530

    List methods

---
+++ mos/List Methods.py
@@ -0,0 +1,13 @@
+numbers = [5, 2, 1, 7, 5, 4]
+
+numbers.append(20)
+# numbers.clear()
+# numbers.pop()
+print(numbers.index(5))
+print(numbers)
+print(numbers.count(5))
+numbers.sort()
+print(numbers)
+
+numbers2 = numbers.copy()
+print(numbers2)
