a5f1671d942f87cceb714a32dc9d9d24f66f5890
pradip <pradipbhattacharjya1997@gmail.com>
2025-08-29 15:54:50 +0530

    For loops

---
+++ mos/For loops.py
@@ -0,0 +1,15 @@
+for item in 'Python':
+    print(item)
+
+
+for item in ['Mosh','John', 'Sarah']:
+    print(item)
+
+for item in range(11):
+    print(item)
+
+for item in range(5, 10):
+    print(item)
+
+for item in range(5, 10,2):
+    print(item)
