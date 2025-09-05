b6d47bd56bfd5b824d560190359af2b8467bd6bc
pradip <pradipbhattacharjya1997@gmail.com>
2025-09-05 09:03:04 +0530

    project transactions

---
+++ mos/project/app.py
@@ -0,0 +1,39 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+wb = xl.load_workbook('transactions.xlsx')
+sheet = wb ['Sheet1']
+cell = sheet['a1']
+cell = sheet.cell(1,1)
+# print(sheet.max_row)
+
+for row in range(1, sheet.max_row + 1):
+    print(row)
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    print(cell.value)
+
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    ected_price = cell.value * 0.9
+    ected_price_cell = sheet.cell(row, 4)
+    ected_price_cell.value = corrected_price
+
+values = Reference(sheet, 
+          min_row=2,
+          max_row=sheet.max_row,
+          min_col=4,
+          max_col=4 )
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
+wb.save('transactions2.xlsx')
