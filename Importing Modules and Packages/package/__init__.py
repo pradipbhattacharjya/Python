499211c0975f5e1361572101860c6a33aa7d4205
pradip <pradipbhattacharjya1997@gmail.com>
2026-02-15 13:54:22 +0530

    Import Modules And Packages.

---
+++ Importing Modules and Packages/package/__init__.py
@@ -0,0 +1,7 @@
+'''__init__.py is a special file in Python that is used to indicate that a directory 
+ 
+is a package. It can be an empty file, but it can also contain initialization code for 
+ the package. When you import a package, the code in __init__.py is executed, allowing you to set 
+ up any necessary variables, functions, 
+ or classes that should be available when 
+ the package is imported.'''
